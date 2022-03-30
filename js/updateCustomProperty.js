ffd0a77f0a40c6941bd439d7ea4555737a6e5210
pdrmenezes <pdrmenezes1@gmail.com>
2022-03-29 22:53:23 -0300

    first commit

---
+++ js/updateCustomProperty.js
@@ -0,0 +1,14 @@
+export function getCustomProperty(element, property) {
+    return parseFloat(getComputedStyle(element).getPropertyValue(property)) || 0
+}
+
+export function setCustomProperty(element, property, value) {
+    element.style.setProperty(property, value)
+
+}
+
+export function incrementCustomProperty(element, property, incrementValue) {
+    setCustomProperty(element, property, getCustomProperty(element, property) + incrementValue)
+
+
+}
